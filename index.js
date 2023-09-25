3f46d8af1898f74503961174cbf1664424081491
Phong <phong@sss.local>
2023-09-25 18:57:14 +0700

    Done binding link

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 function copyCA() {
-    navigator.clipboard.writeText("0x0c802b2f35d9ddB09d9F7829B3C7900796031BD3")
+    navigator.clipboard.writeText("0x2136B437F3025667aA42501Cb08c6a781D2Dbc7B")
 }
